d03f50bd0c8786ea5fe8af71d423abb8924aa854
Vybhav Podala (Digital) <VY323647@wipro.com>
2019-07-18 15:09:14 +0530

    ressolved merge conflicts and updated gitignore

--- example/RNExampleApp/src/utils/utils.js
+++ example/RNExampleApp/src/utils/utils.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const flattenObject = ob => {
   const toReturn = {};
 
@@ -17,25 +16,3 @@ export const flattenObject = ob => {
   }
   return toReturn;
 };
-=======
-export const flattenObject = (ob) => {
-    const toReturn = {};
-
-    for (const i in ob) {
-        if (!ob.hasOwnProperty(i)) continue;
-
-        if ((typeof ob[i]) == 'object' && ob[i] !== null) {
-            const flatObject = flattenObject(ob[i]);
-            for (const x in flatObject) {
-                if (!flatObject.hasOwnProperty(x)) continue;
-
-                toReturn[i + '.' + x] = flatObject[x];
-            }
-        } else {
-            toReturn[i] = ob[i];
-        }
-    }
-    return toReturn;
-};
-
->>>>>>> ed5917714ec20fdb422ea2bcc130f40b6dab51cf
